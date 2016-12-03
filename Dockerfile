fbbdb3e9b836f37d1f8fb8f2c0639c808439afbd
pankaj_dighe <pankajdighe77@gmail.com>
2016-12-03 14:21:27 -0800

    Added chnages

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM resin/rpi-raspbian
+
+#Install Utility
+
+RUN apt-get -q update \
+	&& apt-get -qy install \
+ 		wget \
+		python python-dev python-pip python-virtualenv \
+		build-essential  \
+		curl \
+        	git 
+
+
+RUN pip install paho-mqtt
+RUN pip install RPi.Gpio
+		
+
+WORKDIR /usr/src/app
+
+COPY app/ /usr/src/app
+CMD ["python", "/usr/src/app/main.py"]
---
+++ app/main.py
@@ -0,0 +1,35 @@
+import time
+import paho.mqtt.client as mqtt
+import random
+import os
+import RPi.GPIO as GPIO
+
+
+mqttc=mqtt.Client()
+mqttc.connect("iot.eclipse.org",1883,60)
+mqttc.loop_start()
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN)
+
+#read temperature
+def read_motion_data():
+	current_state = GPIO.input(11)
+	if current_state == 1:
+		result=1;
+	else:
+		print("PIR value is 0")
+		result=0;
+	return result
+
+#publish temperature
+while 1:
+    t=read_motion_data()
+    print "Publishing data"
+    device_uuid=os.environ['RESIN_DEVICE_UUID'];
+    print device_uuid
+    (result,mid)=mqttc.publish("topic/GeneralizedIoT/"+str(device_uuid),t,2)
+    time.sleep(1)
+
+mqttc.loop_stop()
+mqttc.disconnect()
